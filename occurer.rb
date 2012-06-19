f3d2995e3d33a28a337eb7cbc1ddb462ed91c691
Snehal Sakre <sne.ringane@gmail.com>
2012-06-19 10:15:48 +0530

    more improvement

--- occurer.rb
+++ occurer.rb
@@ -36,124 +36,124 @@ grade = gets.chomp.to_i
 
 case grade
 when 1
-a = str.scan("RUBY").count();
-occure = "Occurences of RUBY word in string1 : #{a}";
-puts occure;
+	a = str.scan("RUBY").count();
+	occure = "Occurences of RUBY word in string1 : #{a}";
+	puts occure;
 
 when 2
-positions = str.enum_for(:scan, /RUBY/).map { Regexp.last_match.begin(0) }
-puts "positions of RUBY :";
-puts positions;
+	positions = str.enum_for(:scan, /RUBY/).map { Regexp.last_match.begin(0) }
+	puts "positions of RUBY :";
+	puts positions;
 
 when 3
-myArray = str.split(/ /);
-def recursive_Array(params)
-  return if params.empty?
-  params[0].each do |p|
-    puts p
-    recursive_Array(params[1..-1])
-  end
-end
+	myArray = str.split(/ /);
+	def recursive_Array(params)
+        return if params.empty?
+        params[0].each do |p|
+        puts p
+        recursive_Array(params[1..-1])
+        end
+        end
 
-recursive_Array(myArray)
-puts
+	recursive_Array(myArray)
+	puts
 
 when 4
-puts "Capitalize String:";
-puts str.capitalize;
-puts
+	puts "Capitalize String:";
+	puts str.capitalize;
+	puts
 
 when 5
-puts "Combine two strings";
-puts str+str1
-puts
+	puts "Combine two strings";
+	puts str+str1
+	puts
  
 when 6
-puts "Heredoc";
-heredoc1 = <<-str
-RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code.The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag.RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used.
-str
-puts heredoc1;
+	puts "Heredoc";
+	heredoc1 = <<-str
+	RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code.The parser then executes all of the code it 	finds until it runs into a RUBY closing <br/> tag.RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used.
+	str
+	puts heredoc1;
 
 when 7
-puts "Current Date :";
-puts Time.now.strftime("%d/%m/%Y")
-puts
+	puts "Current Date :";
+	puts Time.now.strftime("%d/%m/%Y")
+	puts
 
 when 8
-start = Time.local(2011,1,12);
-puts start.strftime("%d %b %Y");
-puts
+	start = Time.local(2011,1,12);
+	puts start.strftime("%d %b %Y");
+	puts
 
 when 9
-time=Time.new+(60*60*24*7)
-puts "Add 7 days to current days :";
-puts "#{time.day}/#{time.month}/#{time.year}";
-puts
+	time=Time.new+(60*60*24*7)
+	puts "Add 7 days to current days :";
+	puts "#{time.day}/#{time.month}/#{time.year}";
+	puts
 
 when 10
-puts "Cut string1 into 4 parts :";
-puts str[0,54];
-puts str[55,54];
-puts str[109,54];
-puts str[164,54];
-puts
+	puts "Cut string1 into 4 parts :";
+	puts str[0,54];
+	puts str[55,54];
+	puts str[109,54];
+	puts str[164,54];
+	puts
 
 when 11
-puts "Split the string";
-splitstr1 = str.split(".");
-puts splitstr1;
-puts splitstr1[0].split(" ").reverse.join(" ")+splitstr1[1].split(" ").reverse.join(" ");
-puts
+	puts "Split the string";
+	splitstr1 = str.split(".");
+	puts splitstr1;
+	puts splitstr1[0].split(" ").reverse.join(" ")+splitstr1[1].split(" ").reverse.join(" ");
+	puts
 
 when 12
-puts "Remove HTML character"
-puts str.gsub(/<\/?[^>]*>/, "");
-puts 
+	puts "Remove HTML character"
+	puts str.gsub(/<\/?[^>]*>/, "");
+	puts 
 
 when 13
-puts "Print RUBY word by traversing string1"
-puts str.scan("RUBY");
-puts
-
+	puts "Print RUBY word by traversing string1"
+	puts str.scan("RUBY");
+	puts
+	
 when 14
-puts "Length of string1 and string2"
-puts "Length of String1 : #{str.length}";
-puts "Length of String2 : #{str1.length}";
-puts 
+	puts "Length of string1 and string2"
+	puts "Length of String1 : #{str.length}";
+	puts "Length of String2 : #{str1.length}";
+	puts 
 
 when 15
-puts "Write a string1 into file"
-aFile = File.new("myString.txt", "w")
-aFile.write(str)
-aFile.close
+	puts "Write a string1 into file"
+	aFile = File.new("myString.txt", "w")
+	aFile.write(str)
+	aFile.close
 
 when 16
-puts "Print Globle variables"
-globlevar = global_variables();
-puts globlevar;
-puts
+	puts "Print Globle variables"
+	globlevar = global_variables();
+	puts globlevar;
+	puts
 
 when 19
-puts "Compare two dates"
-start = Time.local(2010,12,4);
-end1 = Time.local(2011,12,5);
-if(end1 > start)
-result = (end1-start)/60/60/24;
-else
-result = (end1-start)/60/60/24;
-end
-puts result;
-puts
+	puts "Compare two dates"
+	start = Time.local(2010,12,4);
+	end1 = Time.local(2011,12,5);
+	if(end1 > start)
+	result = (end1-start)/60/60/24;
+	else
+	result = (end1-start)/60/60/24;
+	end
+	puts result;
+	puts
 
 when 20
-time=Time.new+(60*60*24*20)
-puts "Add 20 days to current days :";
-puts "#{time.day}/#{time.month}/#{time.year}";
-puts
+	time=Time.new+(60*60*24*20)
+	puts "Add 20 days to current days :";
+	puts "#{time.day}/#{time.month}/#{time.year}";
+	puts
 
 when 21
-puts "Print Date in array format :";
-puts Time.now.to_a[0..Time.now.to_a.length]
-puts
-end
+	puts "Print Date in array format :";
+	puts Time.now.to_a[0..Time.now.to_a.length]
+	puts
+	end
